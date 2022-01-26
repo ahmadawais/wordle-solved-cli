418f000c780960994dee7f596fefbdbf0c665145
Ahmad Awais <me@AhmadAwais.com>
2022-01-26 22:14:02 +0500

    📦 NEW: Aaand we are done

--- index.js
+++ index.js
@@ -7,9 +7,12 @@
  * @author Ahmad Awais <https://twitter.com/MrAhmadAwais/>
  */
 
+const alert = require('cli-alerts');
+
 const init = require('./utils/init');
 const cli = require('./utils/cli');
 const log = require('./utils/log');
+const { wordToday, wordYesterday, wordTomorrow } = require('./utils/solve');
 
 const input = cli.input;
 const flags = cli.flags;
@@ -18,6 +21,10 @@ const { clear, debug } = flags;
 (async () => {
 	init({ clear });
 	input.includes(`help`) && cli.showHelp(0);
+
+	alert({ type: `success`, msg: wordToday, name: `TODAY` });
+	alert({ type: `warning`, msg: wordYesterday, name: `YESTERDAY` });
+	alert({ type: `info`, msg: wordTomorrow, name: `TOMORROW` });
 
 	debug && log(flags);
 })();
---
+++ utils/solve.js
@@ -0,0 +1,29 @@
+const wordList = require('./list');
+
+function getDateDifference(e, a) {
+	let s = new Date(e),
+		t = new Date(a).setHours(0, 0, 0, 0) - s.setHours(0, 0, 0, 0);
+	return Math.floor(t / 864e5);
+}
+
+let baseDate = new Date(2021, 5, 19, 0, 0, 0, 0);
+
+function callGetDateDifference(todaysDate) {
+	return getDateDifference(baseDate, todaysDate);
+}
+
+function getWordOfTheDay(today) {
+	let a,
+		s = callGetDateDifference(today);
+	return (a = s % wordList.length), wordList[a];
+}
+
+let today = new Date();
+let yesterday = new Date().setDate(today.getDate() - 1);
+let tomorrow = new Date().setDate(today.getDate() + 1);
+
+const wordToday = getWordOfTheDay(today);
+const wordYesterday = getWordOfTheDay(yesterday);
+const wordTomorrow = getWordOfTheDay(tomorrow);
+
+module.exports = { wordToday, wordYesterday, wordTomorrow };
